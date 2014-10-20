a8eb303422007312844aa9ea9411b3f545ce1bf5
joaqimp@gmail.com <root@localhost.localdomain>
2014-10-20 18:13:41 +0200

    'date'

---
+++ build/src/CMakeFiles/oglfirst.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oglfirst.dir/oglfirst.cpp.o"
+  "oglfirst.pdb"
+  "oglfirst"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oglfirst.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
